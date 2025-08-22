ea726e8a9664c1117771d74cb30c582d4d795617
Hassan-HassanAbdullah <iqasir575@gmail.com>
2025-08-23 02:47:48 +0500

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,10 @@
 export const environment = {
   production: false,
-  authApiUrl: 'http://localhost:3000/api/auth',
-  recipeApiUrl: 'http://localhost:3000/api/recipes',
+  apiUrl: 'https://recipesaverbackend-production.up.railway.app',
+  authApiUrl: 'https://recipesaverbackend-production.up.railway.app/api/auth',
+  recipeApiUrl: 'https://recipesaverbackend-production.up.railway.app/api/recipes',
+  
+  // apiUrl: 'http://localhost:3000',
+  // authApiUrl: 'http://localhost:3000/api/auth',
+  // recipeApiUrl: 'http://localhost:3000/api/recipes',
 };
